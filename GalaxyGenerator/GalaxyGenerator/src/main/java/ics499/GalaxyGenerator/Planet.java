cb315ec51b6945acb7306ca76b9da8d0bd21ff9c
Nguyen Truong <nguyen.truong@vomela.com>
2023-02-01 08:32:59 -0600

    Empty classes and fields

---
+++ GalaxyGenerator/GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/Planet.java
@@ -0,0 +1,103 @@
+package ics499.GalaxyGenerator;
+
+public class Planet {
+  private String name;
+  private String type;
+  private String description;
+  private int population;
+  private int naturalResources;
+  private int economyLevel;
+  private String economyType;
+  private double[][] location;
+
+  public Planet(String name, String type, String description, int population, int naturalResources, int economyLevel, String economyType, double[][] location) {
+    this.name = name;
+    this.type = type;
+    this.description = description;
+    this.population = population;
+    this.naturalResources = naturalResources;
+    this.economyLevel = economyLevel;
+    this.economyType = economyType;
+    this.location = location;
+  }
+
+  public Planet() {
+  }
+
+  public void generate(){
+    /**
+     * TODO
+     */
+  }
+
+  public void onClick(){
+    /**
+     * TODO
+     */
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public int getPopulation() {
+    return this.population;
+  }
+
+  public void setPopulation(int population) {
+    this.population = population;
+  }
+
+  public int getNaturalResources() {
+    return this.naturalResources;
+  }
+
+  public void setNaturalResources(int naturalResources) {
+    this.naturalResources = naturalResources;
+  }
+
+  public int getEconomyLevel() {
+    return this.economyLevel;
+  }
+
+  public void setEconomyLevel(int economyLevel) {
+    this.economyLevel = economyLevel;
+  }
+
+  public String getEconomyType() {
+    return this.economyType;
+  }
+
+  public void setEconomyType(String economyType) {
+    this.economyType = economyType;
+  }
+
+  public double[][] getLocation() {
+    return this.location;
+  }
+
+  public void setLocation(double[][] location) {
+    this.location = location;
+  }
+  
+}
---
+++ GalaxyGenerator/GalaxyGenerator/src/test/java/ics499/GalaxyGenerator/GalaxyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ics499.GalaxyGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
