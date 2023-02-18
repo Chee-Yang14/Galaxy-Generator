74d3696b24bcbb252c0d10f94c9add40ad0fe278
JaroschJr <jaroschjr@gmail.com>
2023-02-17 18:03:49 -0600

    Generates universe-level master list of names.

--- GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/Universe.java
+++ GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/Universe.java
@@ -11,8 +11,11 @@ import jakarta.persistence.Transient;
 public class Universe {
 
   private Integer id;
+  private int seed;
   private String shape;
   private List<StarSystem> starSystem;
+  private Stack<String> names;
+  private Random rand;
 
   public Universe(String shape, List<StarSystem> starSystem){
     this.shape = shape;
@@ -20,8 +23,18 @@ public class Universe {
   }
 
   public Universe(){}
+  
+  public String newName() {
+	  return names.pop();
+  }
 
   public void generate(){
+	  seed = 1; //seed will be given value by input, eventually
+	  rand = new Random(seed);
+	  names = new Stack<String>();
+	  generateNames(1000);
+	  
+	  
     /**
      * TODO 
      */
@@ -41,5 +54,32 @@ public class Universe {
 
   public void setStarSystem(List<StarSystem> starSystem) {
     this.starSystem = starSystem;
+  }
+  
+  private void generateNames(int number) {
+	String[] firstSylable = {"Ame","Shi", "Kiin", "Kael", "Sal","Sale", "Her", "Hur", "Hue", "New",
+			"Old", "Bri", "Twi", "Kel", "Lit", "Le","Lye", "Deep", "Dark", "Alt", "Ber", "Bres", "Sat", "Fal",
+			"Ka", "Ca", "Sej", "Con"};
+	String[] secondSylable = {"rash", "nar", "'kan", "kan", "sho", "del", "le","la","ta","coru","mel",
+			"'coru", "sin", "shin", "ba","sed","dro"};
+	String[] thirdSylable = {"shin", "shen", "dale", "dreg", "je", "se", "ae", "te","to","toe","ri",
+			"dro", "dra","jra","del","rel","ni"};
+
+	for(int i = 0; i<number; i++) {
+		int num = rand.nextInt(3);
+		if(num == 0) {
+			String name = firstSylable[rand.nextInt(firstSylable.length)];
+			names.push(name);
+		}else if(num == 1) {
+			String name = firstSylable[rand.nextInt(firstSylable.length)] + secondSylable[rand.nextInt(secondSylable.length)];
+			names.push(name);
+		}else if(num == 2) {
+			String name = firstSylable[rand.nextInt(firstSylable.length)] + secondSylable[rand.nextInt(secondSylable.length)] + thirdSylable[rand.nextInt(thirdSylable.length)];
+			names.push(name);
+		}else {
+			System.out.println("Ranom number somehow out of range");
+		}
+		
+	}
   }
 }
