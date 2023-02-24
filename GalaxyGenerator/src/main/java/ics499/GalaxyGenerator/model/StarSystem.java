89bee918cbcbc619a13c17edd2e882ef88e0d464
lamtruong <nl2791999@gmail.com>
2023-02-24 14:42:41 -0600

    change planet description

--- GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/StarSystem.java
+++ GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/StarSystem.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name="starsystem")
+@Table(name = "starsystem")
 public class StarSystem {
   @Id
   @GeneratedValue
@@ -28,10 +28,10 @@ public class StarSystem {
   private int spaceResources;
   @Transient
   private List<Planet> planets;
-  private int[] location;//X, Y
+  private int[] location;// X, Y
 
-
-  public StarSystem(String name, StarType type, String goverment, long population, int economyLevel, int spaceResources, List<Planet> planets, int[] location) {
+  public StarSystem(String name, StarType type, String goverment, long population, int economyLevel, int spaceResources,
+      List<Planet> planets, int[] location) {
     this.name = name;
     this.type = type;
     this.goverment = goverment;
@@ -41,61 +41,65 @@ public class StarSystem {
     this.planets = planets;
     this.location = location;
   }
-  
+
   public StarSystem(Universe u) {
-	  GalaxyShape shape = u.getShape();
-	  Random r = u.getRandom();
-	  if(shape.equals(GalaxyShape.SCATTER)) {
-		  location = new int[] {r.nextInt(100), r.nextInt(100)};
-	  }else if(shape.equals(GalaxyShape.CLUSTER)) {
-		  location = new int[] {r.nextInt(50) + r.nextInt(50), r.nextInt(50) + r.nextInt(50)};
-		  
-	  }
-	  
-	  int starTableRoll = r.nextInt(1000);	//The real probabilities of the star types can be found here https://en.wikipedia.org/wiki/Stellar_classification
-	  if(starTableRoll < 450) {				//I have fudged the numbers a little bit for a bir more even a spread.
-		 this.type = StarType.M;
-	  }else if (starTableRoll < 750 ) {
-		  this.type = StarType.K;
-	  }else if (starTableRoll < 900 ) {
-		  this.type = StarType.G;
-	  }else if (starTableRoll < 950 ) {
-		  this.type = StarType.F;
-	  }else if (starTableRoll < 990 ) {
-		  this.type = StarType.A;
-	  }else if (starTableRoll < 996 ) {
-		  this.type = StarType.B;
-	  }else {
-		  this.type = StarType.O;
-	  }
-	  
-	  this.population = 0;
-	  this.economyLevel = 0;
-	  int sizeA = r.nextInt(10);
-	  int sizeB = r.nextInt(10);
-	  int iPlanets = Math.min(sizeA, sizeB); //roll two 10-sided dice, take the lower. There could be up to 12 planets, with the average being aprox 4.5
-	  for(int i = 0; i<iPlanets; i++) {
-		  Planet newPlanet = Planet.generate(r);
-		  this.population += newPlanet.getPopulation();
-		  this.economyLevel += newPlanet.getEconomyLevel();
-		  planets.add(newPlanet);
-	  }
-	  //In the future, we may want the system totals to be higher than the sum of it's planets. We will need to work out the formula.
-	  
+    GalaxyShape shape = u.getShape();
+    Random r = u.getRandom();
+    if (shape.equals(GalaxyShape.SCATTER)) {
+      location = new int[] { r.nextInt(100), r.nextInt(100) };
+    } else if (shape.equals(GalaxyShape.CLUSTER)) {
+      location = new int[] { r.nextInt(50) + r.nextInt(50), r.nextInt(50) + r.nextInt(50) };
+
+    }
+
+    int starTableRoll = r.nextInt(1000); // The real probabilities of the star types can be found here
+                                         // https://en.wikipedia.org/wiki/Stellar_classification
+    if (starTableRoll < 450) { // I have fudged the numbers a little bit for a bir more even a spread.
+      this.type = StarType.M;
+    } else if (starTableRoll < 750) {
+      this.type = StarType.K;
+    } else if (starTableRoll < 900) {
+      this.type = StarType.G;
+    } else if (starTableRoll < 950) {
+      this.type = StarType.F;
+    } else if (starTableRoll < 990) {
+      this.type = StarType.A;
+    } else if (starTableRoll < 996) {
+      this.type = StarType.B;
+    } else {
+      this.type = StarType.O;
+    }
+
+    this.population = 0;
+    this.economyLevel = 0;
+    int sizeA = r.nextInt(10);
+    int sizeB = r.nextInt(10);
+    int iPlanets = Math.min(sizeA, sizeB); // roll two 10-sided dice, take the lower. There could be up to 12 planets,
+                                           // with the average being aprox 4.5
+    for (int i = 0; i < iPlanets; i++) {
+      Planet newPlanet = Planet.generate(r);
+      this.population += newPlanet.getPopulation();
+      this.economyLevel += newPlanet.getEconomyLevel();
+      planets.add(newPlanet);
+    }
+    // In the future, we may want the system totals to be higher than the sum of
+    // it's planets. We will need to work out the formula.
+
   }
 
-  public StarSystem(){}
+  public StarSystem() {
+  }
 
-  public static StarSystem generate(Universe u){
-	  return new StarSystem(u);
+  public static StarSystem generate(Universe u) {
+    return new StarSystem(u);
     /**
-     * TODO 
+     * TODO
      */
   }
 
-  public void onClick(){
+  public void onClick() {
     /**
-     * TODO 
+     * TODO
      */
   }
 
--- GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/Universe.java
+++ GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/Universe.java
@@ -10,8 +10,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+
 @Entity
-@Table(name="Universe")
+@Table(name = "Universe")
 public class Universe {
   @Id
   @GeneratedValue
@@ -25,42 +26,76 @@ public class Universe {
   @Transient
   private Random rand;
 
-  public Universe(GalaxyShape shape, Random randoom, int size, int seed){
+  public Universe(GalaxyShape shape, Random randoom, int size, int seed) {
     this.shape = shape;
-    //this.starSystem = starSystem;
+    // this.starSystem = starSystem;
     this.rand = randoom;
     this.names = new Stack<String>();
-	generateNames(1000);
-	for(int i = 0; i < size; i++) {
-		starSystem.add(StarSystem.generate(this));
-	}
-	
+    generateNames(1000);
+    for (int i = 0; i < size; i++) {
+      starSystem.add(StarSystem.generate(this));
+    }
   }
 
-  public Universe(){}
-  
+  public Universe() {
+  }
+
   public String newName() {
-	  if(names.size()<=2) {
-		  generateNames(1000);
-	  }
-	  return names.pop();
-  }
-  
-  public Random getRandom() {
-	  return rand;
+    if (names.size() <= 2) {
+      generateNames(1000);
+    }
+    return names.pop();
   }
 
-  public static Universe generate(int randSeed, int gSize, GalaxyShape gShape){
-	  //seed will be given value by input, eventually
-	  Random nRandom = new Random(randSeed);
-	  Universe u = new Universe(gShape, nRandom, gSize, randSeed );
-	  return u;
-	  
-	  
-	  
+  public Random getRandom() {
+    return rand;
+  }
+
+  public static Universe generate(int randSeed, int gSize, GalaxyShape gShape) {
+    // seed will be given value by input, eventually
+    Random nRandom = new Random(randSeed);
+    Universe u = new Universe(gShape, nRandom, gSize, randSeed);
+    return u;
+
     /**
-     * TODO 
+     * TODO
      */
+  }
+
+  private void generateNames(int number) {
+    String[] firstSylable = { "Ame", "Shi", "Kiin", "Kael", "Sal", "Sale", "Her", "Hur", "Hue", "New",
+        "Old", "Bri", "Twi", "Kel", "Lit", "Le", "Lye", "Deep", "Dark", "Alt", "Ber", "Bres", "Sat", "Fal",
+        "Ka", "Ca", "Sej", "Con" };
+    String[] secondSylable = { "rash", "nar", "'kan", "kan", "sho", "del", "le", "la", "ta", "coru", "mel",
+        "'coru", "sin", "shin", "ba", "sed", "dro" };
+    String[] thirdSylable = { "shin", "shen", "dale", "dreg", "je", "se", "ae", "te", "to", "toe", "ri",
+        "dro", "dra", "jra", "del", "rel", "ni" };
+
+    for (int i = 0; i < number; i++) {
+      int num = rand.nextInt(3);
+      if (num == 0) {
+        String name = firstSylable[rand.nextInt(firstSylable.length)];
+        names.push(name);
+      } else if (num == 1) {
+        String name = firstSylable[rand.nextInt(firstSylable.length)]
+            + secondSylable[rand.nextInt(secondSylable.length)];
+        names.push(name);
+      } else if (num == 2) {
+        String name = firstSylable[rand.nextInt(firstSylable.length)]
+            + secondSylable[rand.nextInt(secondSylable.length)] + thirdSylable[rand.nextInt(thirdSylable.length)];
+        names.push(name);
+      } else {
+        System.out.println("Ranom number somehow out of range");
+      }
+    }
+  }
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
   }
 
   public GalaxyShape getShape() {
@@ -77,32 +112,5 @@ public class Universe {
 
   public void setStarSystem(List<StarSystem> starSystem) {
     this.starSystem = starSystem;
-  }
-  
-  private void generateNames(int number) {
-	String[] firstSylable = {"Ame","Shi", "Kiin", "Kael", "Sal","Sale", "Her", "Hur", "Hue", "New",
-			"Old", "Bri", "Twi", "Kel", "Lit", "Le","Lye", "Deep", "Dark", "Alt", "Ber", "Bres", "Sat", "Fal",
-			"Ka", "Ca", "Sej", "Con"};
-	String[] secondSylable = {"rash", "nar", "'kan", "kan", "sho", "del", "le","la","ta","coru","mel",
-			"'coru", "sin", "shin", "ba","sed","dro"};
-	String[] thirdSylable = {"shin", "shen", "dale", "dreg", "je", "se", "ae", "te","to","toe","ri",
-			"dro", "dra","jra","del","rel","ni"};
-
-	for(int i = 0; i<number; i++) {
-		int num = rand.nextInt(3);
-		if(num == 0) {
-			String name = firstSylable[rand.nextInt(firstSylable.length)];
-			names.push(name);
-		}else if(num == 1) {
-			String name = firstSylable[rand.nextInt(firstSylable.length)] + secondSylable[rand.nextInt(secondSylable.length)];
-			names.push(name);
-		}else if(num == 2) {
-			String name = firstSylable[rand.nextInt(firstSylable.length)] + secondSylable[rand.nextInt(secondSylable.length)] + thirdSylable[rand.nextInt(thirdSylable.length)];
-			names.push(name);
-		}else {
-			System.out.println("Ranom number somehow out of range");
-		}
-		
-	}
   }
 }
