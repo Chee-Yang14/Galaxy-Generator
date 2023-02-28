74aff5e8e21d4e262dc991c5899f663c83cead96
Andy <kingp@10.0.0.114>
2023-02-27 21:22:35 -0600

    create a method for each random generation.

--- GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/StarSystem.java
+++ GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/StarSystem.java
@@ -18,175 +18,306 @@ import jakarta.persistence.Transient;
 @Entity
 @Table(name = "starsystem")
 public class StarSystem {
-  @Id
-  @GeneratedValue
-  @SequenceGenerator(name = "starsystem", allocationSize = 1)
-  private Integer starsystemId;
-  @Transient
-  private Universe u;
-  private Random rand;
-  private String name;
-  private StarType type;
-  private String goverment;
-  private long population;
-  private int economyLevel;
-  private int spaceResources;
-  @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-  private List<Planet> planets = new ArrayList<Planet>();
-  private int[] location;// X, Y
+	@Id
+	@GeneratedValue
+	@SequenceGenerator(name = "starsystem", allocationSize = 1)
+	private Integer starsystemId;
+	@Transient
+	private Universe u;
+	private Random random = new Random();
+	private String name;
+	private StarType type;
+	private String goverment;
+	private long population;
+	private int economyLevel;
+	private int spaceResources;
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private List<Planet> planets = new ArrayList<Planet>();
+	private int[] location;// X, Y
 
-  public StarSystem(String name, StarType type, String goverment, long population, int economyLevel, int spaceResources,
-      List<Planet> planets, int[] location) {
-    this.name = name;
-    this.type = type;
-    this.goverment = goverment;
-    this.population = population;
-    this.economyLevel = economyLevel;
-    this.spaceResources = spaceResources;
-    this.planets = planets;
-    this.location = location;
-  }
+	public StarSystem(String name, StarType type, String goverment, long population, int economyLevel, int spaceResources,
+			List<Planet> planets, int[] location) {
+		this.name = name;
+		this.type = type;
+		this.goverment = goverment;
+		this.population = population;
+		this.economyLevel = economyLevel;
+		this.spaceResources = spaceResources;
+		this.planets = planets;
+		this.location = location;
+	}
 
-  public StarSystem(Universe u) {
-    GalaxyShape shape = u.getShape();
-    Random r = u.getRandom();
-    if (shape.equals(GalaxyShape.SCATTER)) {
-      location = new int[] { r.nextInt(100), r.nextInt(100) };
-    } else if (shape.equals(GalaxyShape.CLUSTER)) {
-      location = new int[] { r.nextInt(50) + r.nextInt(50), r.nextInt(50) + r.nextInt(50) };
+	public StarSystem(Universe u) {
+		int sizeA = random.nextInt(10);
+		int sizeB = random.nextInt(10);
+		int iPlanets = Math.min(sizeA, sizeB); // roll two 10-sided dice, take the lower. There could be up to 12 planets,
+		// with the average being aprox 4.5
+		this.setUniverse(u);
+		this.setName(generateNames());
+		this.setType(createStarType());
+		this.setLocation(createLocation());
+		this.setPlanets(createPlanets(iPlanets));
+		this.setPopulation(createPopulation());
+		this.setEconomyLevel(createEconLevel());
+		this.setSpaceResources(createSpaceResource());
+		this.setGoverment(createGovernment());
+		// In the future, we may want the system totals to be higher than the sum of
+		// it's planets. We will need to work out the formula.
 
-    }
+	}
 
-    int starTableRoll = r.nextInt(1000); // The real probabilities of the star types can be found here
-                                         // https://en.wikipedia.org/wiki/Stellar_classification
-    if (starTableRoll < 450) { // I have fudged the numbers a little bit for a bir more even a spread.
-      this.type = StarType.M;
-    } else if (starTableRoll < 750) {
-      this.type = StarType.K;
-    } else if (starTableRoll < 900) {
-      this.type = StarType.G;
-    } else if (starTableRoll < 950) {
-      this.type = StarType.F;
-    } else if (starTableRoll < 990) {
-      this.type = StarType.A;
-    } else if (starTableRoll < 996) {
-      this.type = StarType.B;
-    } else {
-      this.type = StarType.O;
-    }
+	public StarSystem() {
+		int sizeA = random.nextInt(10);
+		int sizeB = random.nextInt(10);
+		int iPlanets = Math.min(sizeA, sizeB); // roll two 10-sided dice, take the lower. There could be up to 12 planets,
+		// with the average being aprox 4.5
+		this.setName(generateNames());
+		this.setType(createStarType());
+		this.setLocation(createLocation());
+		this.setPlanets(createPlanets(iPlanets));
+		this.setPopulation(createPopulation());
+		this.setEconomyLevel(createEconLevel());
+		this.setSpaceResources(createSpaceResource());
+		this.setGoverment(createGovernment());
+	}
 
-    this.population = 0;
-    this.economyLevel = 0;
-    int sizeA = r.nextInt(10);
-    int sizeB = r.nextInt(10);
-    int iPlanets = Math.min(sizeA, sizeB); // roll two 10-sided dice, take the lower. There could be up to 12 planets,
-                                           // with the average being aprox 4.5
-    for (int i = 0; i < iPlanets; i++) {
-      Planet newPlanet = Planet.generate(r);
-      this.population += newPlanet.getPopulation();
-      this.economyLevel += newPlanet.getEconomyLevel();
-      planets.add(newPlanet);
-    }
-    // In the future, we may want the system totals to be higher than the sum of
-    // it's planets. We will need to work out the formula.
+	public static StarSystem generate(Universe u) {
+		return new StarSystem(u);
+		/**
+		 * TODO
+		 */
+	}
 
-  }
+	public void onClick() {
+		/**
+		 * TODO
+		 */
+	}
 
-  public StarSystem() {
-  }
+	public String createGovernment() {
+		int num = random.nextInt(10);
+		String government = "";
+		switch (num) {
+			case 1:
+				government = "Democracy";
+				break;
+			case 2:
+				government = "Communism";
+				break;
+			case 3:
+				government = "Socialism";
+				break;
+			case 4:
+				government = "Oligarchy";
+				break;
+			case 5:
+				government = "Aristocracy";
+				break;
+			case 6:
+				government = "Monarchy";
+				break;
+			case 7:
+				government = "Theocracy";
+				break;
+			case 8:
+				government = "Colonialism";
+				break;
+			case 9:
+				government = "Totalitarianism";
+				break;
+			case 10:
+				government = "Military Dictatorship";
+				break;
+		}
+		return government;
+	}
+	
+	public int[] createLocation() {
+		int[] location = {};
+		try {
+			GalaxyShape shape = u.getShape();
+			if (shape.equals(GalaxyShape.SCATTER)) {
+				location = new int[] { random.nextInt(100), random.nextInt(100) };
+			} 
+			else if (shape.equals(GalaxyShape.CLUSTER)) {
+				location = new int[] { random.nextInt(50) + random.nextInt(50), random.nextInt(50) + random.nextInt(50) };
+			}
+		}
+		catch (Exception e) {
+		}
+		return location;
+	}
 
-  public static StarSystem generate(Universe u) {
-    return new StarSystem(u);
-    /**
-     * TODO
-     */
-  }
+	public StarType createStarType() {
+		int starTableRoll = random.nextInt(1000); // The real probabilities of the star types can be found here
+		StarType type = null;
+		// https://en.wikipedia.org/wiki/Stellar_classification
+		if (starTableRoll < 450) { // I have fudged the numbers a little bit for a bir more even a spread.
+			type = StarType.M;
+		} 
+		else if (starTableRoll < 750) {
+			type = StarType.K;
+		} 
+		else if (starTableRoll < 900) {
+			type = StarType.G;
+		} 
+		else if (starTableRoll < 950) {
+			type = StarType.F;
+		} else if (starTableRoll < 990) {
+			type = StarType.A;
+		} else if (starTableRoll < 996) {
+			type = StarType.B;
+		} 
+		else {
+			type = StarType.O;
+		}
+		return type;
+	}
 
-  public void onClick() {
-    /**
-     * TODO
-     */
-  }
+	public long createPopulation() {
+		long population = 0;
+		for (int i = 0; i < planets.size(); i++) {
+			population += planets.get(i).getPopulation();
+		}
+		return population;
+	}
 
-  public Integer getStarsystemId() {
-    return this.starsystemId;
-  }
+	public int createEconLevel() {
+		int level = 0;
+		for (int i = 0; i < planets.size(); i++) {
+			level += planets.get(i).getEconomyLevel();
+		}
+		return level;
+	}
 
-  public void setStarsystemId(Integer id) {
-    this.starsystemId = id;
-  }
+	public ArrayList<Planet> createPlanets(int num) {
+		ArrayList<Planet> planetList = new ArrayList<>();
+		for (int i = 0; i < num; i++) {
+			Planet newPlanet = Planet.generate(random);
+			planetList.add(newPlanet);
+		}
+		return planetList;
+	}
 
-  public String getName() {
-    return this.name;
-  }
+	public int createSpaceResource() {
+		int resource = 0;
+		for (int i = 0; i < planets.size(); i++) {
+			resource += planets.get(i).getNaturalResources();
+		}
+		return resource;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	private String generateNames() {
+		String name = "";
+		String[] firstSylable = { "Ame", "Shi", "Kiin", "Kael", "Sal", "Sale", "Her", "Hur", "Hue", "New",
+				"Old", "Bri", "Twi", "Kel", "Lit", "Le", "Lye", "Deep", "Dark", "Alt", "Ber", "Bres", "Sat", "Fal",
+				"Ka", "Ca", "Sej", "Con" };
+		String[] secondSylable = { "rash", "nar", "'kan", "kan", "sho", "del", "le", "la", "ta", "coru", "mel",
+				"'coru", "sin", "shin", "ba", "sed", "dro" };
+		String[] thirdSylable = { "shin", "shen", "dale", "dreg", "je", "se", "ae", "te", "to", "toe", "ri",
+				"dro", "dra", "jra", "del", "rel", "ni" };
 
-  public StarType getType() {
-    return this.type;
-  }
+		int num = random.nextInt(3);
+		if (num == 0) {
+			name = firstSylable[random.nextInt(firstSylable.length)];
+		} else if (num == 1) {
+			name = firstSylable[random.nextInt(firstSylable.length)] + secondSylable[random.nextInt(secondSylable.length)];
+		} else if (num == 2) {
+			name = firstSylable[random.nextInt(firstSylable.length)] + secondSylable[random.nextInt(secondSylable.length)]
+					+ thirdSylable[random.nextInt(thirdSylable.length)];
+		} else {
+			System.out.println("Random number somehow out of range");
+		}
+		return name;
+	}
 
-  public void setType(StarType type) {
-    this.type = type;
-  }
+	public Integer getStarsystemId() {
+		return this.starsystemId;
+	}
 
-  public String getGoverment() {
-    return this.goverment;
-  }
+	public void setStarsystemId(Integer id) {
+		this.starsystemId = id;
+	}
 
-  public void setGoverment(String goverment) {
-    this.goverment = goverment;
-  }
+	public String getName() {
+		return this.name;
+	}
 
-  public long getPopulation() {
-    return this.population;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public void setPopulation(long population) {
-    this.population = population;
-  }
+	public StarType getType() {
+		return this.type;
+	}
 
-  public int getEconomyLevel() {
-    return this.economyLevel;
-  }
+	public void setType(StarType type) {
+		this.type = type;
+	}
 
-  public void setEconomyLevel(int economyLevel) {
-    this.economyLevel = economyLevel;
-  }
+	public String getGoverment() {
+		return this.goverment;
+	}
 
-  public int getSpaceResources() {
-    return this.spaceResources;
-  }
+	public void setGoverment(String goverment) {
+		this.goverment = goverment;
+	}
 
-  public void setSpaceResources(int spaceResources) {
-    this.spaceResources = spaceResources;
-  }
+	public long getPopulation() {
+		return this.population;
+	}
 
-  public List<Planet> getPlanets() {
-    return this.planets;
-  }
+	public void setPopulation(long population) {
+		this.population = population;
+	}
 
-  public void setPlanets(List<Planet> planets) {
-    this.planets = planets;
-  }
+	public int getEconomyLevel() {
+		return this.economyLevel;
+	}
 
-  public int[] getLocation() {
-    return this.location;
-  }
+	public void setEconomyLevel(int economyLevel) {
+		this.economyLevel = economyLevel;
+	}
 
-  public void setLocation(int[] location) {
-    this.location = location;
-  }
+	public int getSpaceResources() {
+		return this.spaceResources;
+	}
 
-@Override
-public String toString() {
-	return "StarSystem [starsystemId=" + starsystemId + ", u=" + u + ", rand=" + rand + ", name=" + name + ", type="
-			+ type + ", goverment=" + goverment + ", population=" + population + ", economyLevel=" + economyLevel
-			+ ", spaceResources=" + spaceResources + ", planets=" + planets + ", location=" + Arrays.toString(location)
-			+ "]";
-}
-  
-  
+	public void setSpaceResources(int spaceResources) {
+		this.spaceResources = spaceResources;
+	}
+
+	public List<Planet> getPlanets() {
+		return this.planets;
+	}
+
+	public void setPlanets(List<Planet> planets) {
+		this.planets = planets;
+	}
+
+	public int[] getLocation() {
+		return this.location;
+	}
+
+	public void setLocation(int[] location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "StarSystem [starsystemId=" + starsystemId + ", u=" + u + ", rand=" + random + ", name=" + name + ", type="
+				+ type + ", goverment=" + goverment + ", population=" + population + ", economyLevel=" + economyLevel
+				+ ", spaceResources=" + spaceResources + ", planets=" + planets + ", location=" + Arrays.toString(location)
+				+ "]";
+	}
+
+	public Universe getUniverse() {
+		return this.u;
+	}
+
+	public void setUniverse(Universe u) {
+		this.u = u;
+	}
+
+
 }
