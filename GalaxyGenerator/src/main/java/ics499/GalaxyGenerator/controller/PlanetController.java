6a74629f9c681e53fc7262786f21596164d2add3
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2023-03-10 16:56:04 -0600

    docu

--- GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/controller/PlanetController.java
+++ GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/controller/PlanetController.java
@@ -20,7 +20,16 @@ import ics499.GalaxyGenerator.model.Planet;
 import ics499.GalaxyGenerator.model.StarSystem;
 import ics499.GalaxyGenerator.repository.PlanetRepository;
 import ics499.GalaxyGenerator.repository.StarSystemRepository;
-
+/**
+ * This planetController class is used to handle HTTP rest. 
+ * This mean it uses http request like GET, PUT ,POST and delete to manipulate data. 
+ * the data being planets from models package. 
+ * 
+ * @author Chee Yang
+ * @author Lam Truong
+ * @author Joseph jarosch
+ * @author andy phan
+ */
 @RestController
 public class PlanetController {
 
@@ -33,7 +42,12 @@ public class PlanetController {
   // public List<Planet> getALlPlanets() {
   // return repo.findAll();
   // }
-
+  /**
+   * this method uses @pathVariable to find an planet with an id that match the given id and then return it.
+   * @param planetId the given id
+   * @return the planet with the given id and httpStatus that say OK meaning sucessful. 
+   * or in the case there is no id match it return just httpStatus that say not found. 
+   */
   @GetMapping("/planet/{id}")
   public ResponseEntity<Planet> getPlanetById(@PathVariable(value = "id") Integer planetId) {
     try {
@@ -43,7 +57,17 @@ public class PlanetController {
       return new ResponseEntity<Planet>(HttpStatus.NOT_FOUND);
     }
   }
-
+  
+  /**
+   * This method add a planet to the repository. 
+   * It does this by taking a given star system id
+   * then searching the star system repository for a match. 
+   * then it add a planet to that star system when a match is found. 
+   * 
+   * @param planetToAdd is the planet to be added
+   * @param starSystemId to determine which starsystem the adding planet is on
+   * @return the repo after saving and flushing. 
+   */
   @PostMapping("/addplanet/{starSystemId}")
   public Planet create(@RequestBody Planet planetToAdd, @PathVariable Integer starSystemId) {
     StarSystem parentStarSystem = starSystemRepo.findById(starSystemId).get();
@@ -51,6 +75,16 @@ public class PlanetController {
     return repo.saveAndFlush(planetToAdd);
   }
 
+  /**
+   * this method update a planet with a given id
+   * a planet and id is given, the method take the id and look for a match in it repostories. 
+   * then it replace the planet with matching id in it repositories with all the features in the given planet. 
+   * updating the planet in the repositories. 
+   * 
+   * @param planetToUpdate the planet you want to update
+   * @param id the planet id
+   * @return return a HTTP status that say OK or NOT_FOUND depending on sucess and failure.
+   */
   @PutMapping("/planet/{id}")
   public ResponseEntity<?> update(@RequestBody Planet planetToUpdate, @PathVariable Integer id) {
     try {
@@ -71,6 +105,12 @@ public class PlanetController {
     }
   }
 
+  /**
+   * this method delete a planet from the repository
+   * it take an id and use it to find and delete a planet in the repositroy
+   * 
+   * @param planetId id of the planet you want to remove 
+   */
   @DeleteMapping("/planet/{id}")
   public void delete(@PathVariable(value = "id") Integer planetId) {
     repo.deleteById(planetId);
