9896ca90ff7cc0ea7e3c676e500a70fcefaeb97b
lamtruong <nl2791999@gmail.com>
2023-03-16 17:49:58 -0500

    added user test

--- GalaxyGenerator/src/test/java/ics499/GalaxyGenerator/GalaxyGeneratorApplicationTests.java
+++ GalaxyGenerator/src/test/java/ics499/GalaxyGenerator/GalaxyGeneratorApplicationTests.java
@@ -9,5 +9,4 @@ class GalaxyGeneratorApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
--- GalaxyGenerator/src/test/java/ics499/GalaxyGenerator/PlanetTest.java
+++ GalaxyGenerator/src/test/java/ics499/GalaxyGenerator/PlanetTest.java
@@ -1,7 +1,6 @@
 package ics499.GalaxyGenerator;
 
 import static org.junit.Assert.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,42 +14,41 @@ import ics499.GalaxyGenerator.model.Planet;
 import ics499.GalaxyGenerator.repository.PlanetRepository;
 import ics499.GalaxyGenerator.repository.StarSystemRepository;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PlanetTest {
-	
+
 	@Autowired
 	private PlanetController controller;
-	
+
 	@Autowired
 	private StarSystemRepository starSystemRepo;
-	
+
 	@Autowired
 	private PlanetRepository repo;
-	
+
 	@Test
-	void testCreatePlanet() throws Exception{
+	void testCreatePlanet() throws Exception {
 		Planet newPlanet = new Planet();
 		Planet planetAdded = controller.create(newPlanet, 1);
 		assertNotNull(planetAdded.getPlanetId());
 		assertSame(newPlanet, planetAdded);
-		
+
 		Planet planet2Added = controller.create(new Planet(), 1);
 		assertNotNull(repo.findById(planet2Added.getPlanetId()));
 		assertTrue(repo.existsById(planet2Added.getPlanetId()));
 	}
-	
+
 	@Test
 	void testGetPlanet() {
 		ResponseEntity<Planet> response = controller.getPlanetById(1);
 		assertNotNull(controller.getPlanetById(1));
 		assertSame(HttpStatus.OK, response.getStatusCode());
-		
+
 		response = controller.getPlanetById(200);
 		assertSame(HttpStatus.NOT_FOUND, response.getStatusCode());
 	}
-	
+
 	@Test
 	void testPutPlanet() {
 		Planet newPlanet = new Planet();
@@ -58,7 +56,7 @@ class PlanetTest {
 		ResponseEntity<?> response = controller.update(newPlanet, 1);
 		assertSame(HttpStatus.OK, response.getStatusCode());
 		assertSame("Update successfully", response.getBody());
-		
+
 		ResponseEntity<Planet> responsePlanetChanged = controller.getPlanetById(1);
 		assertNotSame(responsePlanet, responsePlanetChanged);
 	}
