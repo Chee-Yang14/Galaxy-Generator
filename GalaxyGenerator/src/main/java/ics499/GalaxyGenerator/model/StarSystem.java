c75d741146f213d804eb022f3e488931fc96f8b0
Andy <kingp@10.0.0.114>
2023-03-31 20:37:29 -0500

    minor update

--- GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/StarSystem.java
+++ GalaxyGenerator/src/main/java/ics499/GalaxyGenerator/model/StarSystem.java
@@ -38,10 +38,7 @@ public class StarSystem {
 
 	@Transient
 	private Universe u;
-	
-	@Transient
 	private boolean capital;
-	
 	@Transient
 	private StarSystem vassalTo;
 	private Random random = new Random();
